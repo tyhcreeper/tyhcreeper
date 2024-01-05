1974f2c3cb65a3714dbe103c061afbddaaf734ef
qa <qa>
2024-01-05 20:13:46 +0800

    Initial Commit

---
+++ qq/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  qq
+//
+//  Created by 1a on 2024/1/5.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
